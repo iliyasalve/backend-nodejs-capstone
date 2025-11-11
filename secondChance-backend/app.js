85632a631ddd70b05d3ccb255ef26c63d570e97f
iliyasalve <iliyasalve@gmail.com>
2025-11-11 17:50:42 -0500

    Create API for SecondChanceItems Service

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
+const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
 
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
@@ -27,7 +28,7 @@ app.use(express.json());
 //{{insert code here}}
 
 // Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
-//{{insert code here}}
+app.use('/api/secondchance/items', secondChanceItemsRoutes);
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
